68705d279af4e645aea6e1ac91ad37b0825fb6a1
Alex-PC <alex_mar1999@yahoo.com>
2023-12-26 13:58:27 +0200

    road object

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const context = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(context);
 
 animate();
@@ -10,6 +11,13 @@ animate();
 function animate(){
     car.update();
     canvas.height = window.innerHeight;
+
+    context.save();
+    context.translate(0, -car.y + canvas.height*0.7);
+
+    road.draw(context);
     car.draw(context);
+
+    context.restore();
     requestAnimationFrame(animate);
 }
